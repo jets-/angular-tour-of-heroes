8e88e2e2931b2731e48bd174c7611a8442202837
Eduardo Trindade <eduardotrinn@gmail.com>
2017-08-01 09:42:25 -0300

    Http Service (create mock heroes)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -35,6 +35,14 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name : string) : Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), {headers: this.headers})
+      .toPromise()
+      .then(resposta => resposta.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   private handleError(error : any) : Promise<any> {
     console.error('An error ocurred', error);
     return Promise.reject(error.message || error);
